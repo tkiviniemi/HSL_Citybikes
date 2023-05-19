a44a0b7eaab483db65c23a428886849895556076
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-19 17:28:59 +0300

    db connection pool created

---
+++ backend/enviroment.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      DB_HOST: string;
+      DB_USER: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+      DB_PORT: string;
+    }
+  }
+}
+
+export {};
