3a03deffca439906c9ad7c95f268edf95fd026d6
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-20 02:42:17 +0300

    Fixed importing env variables

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,7 @@
 import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
 
 import journeysRouter from './routes/journeys.routes';
 import stationsRouter from './routes/stations.routes';
