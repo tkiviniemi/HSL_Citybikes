7114ae4338467448e0be7f521c8f9e42272973e7
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-22 13:14:31 +0300

    QueryClient and provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Home from './pages/Home';
 import Journeys from './pages/Journeys';
 import Stations from './pages/Stations';
 import Navbar from './components/Navbar';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/journeys" element={<Journeys />} />
-        <Route path="/stations" element={<Stations />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/journeys" element={<Journeys />} />
+          <Route path="/stations" element={<Stations />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
