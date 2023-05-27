f1a40e78a5630d5e5ddfb516f4f2b7fb6033708d
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-27 18:04:15 +0300

    Added departure and return times, added 1 decimal to distance calc

--- frontend/src/components/JourneyListItem.tsx
+++ frontend/src/components/JourneyListItem.tsx
@@ -1,13 +1,20 @@
 import { Journey } from '../interfaces/journey.interface';
 
 function JourneyListItem({ journey }: { journey: Journey }) {
+  const departure_time = new Date(journey.departure_time).toLocaleString();
+  const return_time = new Date(journey.return_time).toLocaleString();
+  const covered_distance = (journey.covered_distance / 1000).toFixed(1);
+  const duration = Math.round(journey.duration / 60);
+
   return (
     <tr className="flex justify-between hover:bg-blue-200">
       <td>{journey.id}</td>
       <td>{journey.departure_station_name}</td>
+      <td>{departure_time}</td>
       <td>{journey.return_station_name}</td>
-      <td>{Math.round(journey.covered_distance / 1000)} km</td>
-      <td>{Math.round(journey.duration / 60)} min</td>
+      <td>{return_time}</td>
+      <td>{covered_distance} km</td>
+      <td>{duration} min</td>
     </tr>
   );
 }
