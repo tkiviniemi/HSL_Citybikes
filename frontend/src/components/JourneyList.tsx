54e42a73dd98b57327056693bd8a5ebecfe3e612
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-27 17:58:51 +0300

    Separated List header to its own component

--- frontend/src/components/JourneyList.tsx
+++ frontend/src/components/JourneyList.tsx
@@ -1,5 +1,6 @@
 import JourneyListItem from './JourneyListItem';
 import { Journey } from '../interfaces/journey.interface';
+import ListHeader from './ListHeader';
 
 type SortKey = keyof Journey;
 
@@ -13,37 +14,7 @@ function JourneyList({
   return (
     <div className="flex justify-center">
       <table>
-        <thead className="">
-          <tr className="flex flex-row font-bold gap-4">
-            <th id="id">
-              <button onClick={() => handleSortingChange('id')}>Id</button>
-            </th>
-            <th id="departure_station_name">
-              <button
-                onClick={() => handleSortingChange('departure_station_name')}
-              >
-                Departure station
-              </button>
-            </th>
-            <th id="return_station_name">
-              <button
-                onClick={() => handleSortingChange('return_station_name')}
-              >
-                Return station
-              </button>
-            </th>
-            <th id="covered_distance">
-              <button onClick={() => handleSortingChange('covered_distance')}>
-                Distance
-              </button>
-            </th>
-            <th id="duration">
-              <button onClick={() => handleSortingChange('duration')}>
-                Duration
-              </button>
-            </th>
-          </tr>
-        </thead>
+        <ListHeader handleSortingChange={handleSortingChange} />
         <tbody className="flex flex-col">
           {journeyData.map((journey: Journey) => (
             <JourneyListItem key={journey.id} journey={journey} />
---
+++ frontend/src/components/ListHeader.tsx
@@ -0,0 +1,36 @@
+import { Journey } from '../interfaces/journey.interface';
+type SortKey = keyof Journey;
+
+function ListHeader({
+  handleSortingChange,
+}: {
+  handleSortingChange: (key: SortKey) => void;
+}) {
+  // THESE HARDCODED KEYS AREN'T OPTIMAL, BUT IT'S A STARTING POINT
+  const keys = [
+    { id: 'id', name: 'ID' },
+    { id: 'departure_station_name', name: 'Departure Station' },
+    { id: 'departure_time', name: 'Departure Time' },
+    { id: 'return_station_name', name: 'Return Station' },
+    { id: 'return_time', name: 'Return Time' },
+    { id: 'covered_distance', name: 'Covered Distance' },
+    { id: 'duration', name: 'Duration' },
+  ];
+
+  return (
+    <thead>
+      <tr className="flex flex-row font-bold gap-4">
+        {keys.map((key) => (
+          <th
+            key={key.id}
+            onClick={() => handleSortingChange(key.id as keyof Journey)}
+          >
+            {key.name}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+}
+
+export default ListHeader;
