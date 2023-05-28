e5ad771e08b60e2cb9620c990a342ec269a220c2
Tuomas Kiviniemi <tuomas.kiviniemi@gmail.com>
2023-05-28 23:27:04 +0300

    Interface for stations

---
+++ frontend/src/interfaces/station.interface.ts
@@ -0,0 +1,13 @@
+export interface Station {
+  station_id: number;
+  name_fi: string;
+  name_sv: string;
+  name_en: string;
+  address_fi: string;
+  address_sv: string;
+  city_fi: string;
+  city_sv: string;
+  capacity: number;
+  long: number;
+  lat: number;
+}
